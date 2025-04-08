962fe02faea21f691bc26dcb49fb6c4e7731ee62
BerissaMuyizere <muyizereberissa@gmail.com>
2025-04-08 11:11:36 +0300

    filtering positive and negative numbers

--- index.js
+++ index.js
@@ -12,13 +12,26 @@ console.log(arrayOfStrings(["berissa", "kiwi", "pawpaw", "banana"]))
 function numOperations( arrayOfNumbers){
     arrayOfNumbers.forEach((item, index) =>{
         if(index < 4){
-            // let result = item*8
-            // return result
-            console.log()
-        }else(index >= arrayOfNumbers.length-2);{
-            let result = item +5
-            return result
+            console.log(`${item *8}`)
+        }else if(index >= arrayOfNumbers.length-2);{
+            console.log(`${item+ 5}`)
+            
         }
     })
 }
 numOperations([1,2,3,4,5,6,7])
+
+
+function numbers(nums){
+    nums.filter(( num => {
+        if(num > 0){
+            console.log("positive")
+        } else{
+            console.log("negative")
+        }
+    }))
+}
+numbers([1,3,2,4,5,6,-6,-8])
+
+    
+
